6b73b5e5326c2e891b4e28b4d290c3e405a92325
xiwangly2 <1334850101@qq.com>
2023-06-08 18:31:02 +0800

    玩不动了

--- src/main/java/com/javachat/Client.java
+++ src/main/java/com/javachat/Client.java
@@ -24,33 +24,46 @@ public class Client {
     }
 
     private static void connectToServer() throws IOException {
-        Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
-        if (socket.isConnected()) {
-            System.out.println("成功连接到服务器...");
-        }
-
-        BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter serverWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-
-        Thread serverThread = new Thread(() -> {
+        boolean connected = false;
+        while (!connected) {
             try {
-                String serverMessage;
-                while ((serverMessage = serverReader.readLine()) != null) {
-                    System.out.println("收到服务器消息: " + serverMessage);
+                Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+                if (socket.isConnected()) {
+                    System.out.println("成功连接到服务器...");
+                    connected = true;
                 }
+
+                BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter serverWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+
+                Thread serverThread = new Thread(() -> {
+                    try {
+                        String serverMessage;
+                        while ((serverMessage = serverReader.readLine()) != null) {
+                            System.out.println("收到服务器消息: " + serverMessage);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                serverThread.start();
+
+                BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
+                String userInput;
+                while ((userInput = userInputReader.readLine()) != null) {
+                    serverWriter.println(userInput);
+                    System.out.println("已发送消息给服务器: " + userInput);
+                }
+
+                socket.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                System.out.println("无法连接到服务器，尝试重新连接...");
+                try {
+                    Thread.sleep(3000); // 休眠3秒后尝试重新连接
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
             }
-        });
-        serverThread.start();
-
-        BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
-        String userInput;
-        while ((userInput = userInputReader.readLine()) != null) {
-            serverWriter.println(userInput);
-            System.out.println("已发送消息给服务器: " + userInput);
         }
-
-        socket.close();
     }
 }
