6e8bbd66ec17735bd14a640dc529f73de982e463
xiwangly2 <1334850101@qq.com>
2023-06-08 18:16:25 +0800

    玩不动了

---
+++ src/main/java/com/javachat/Main.java
@@ -0,0 +1,8 @@
+package com.javachat;
+
+public class Main {
+    public static void main(String[] args) {
+        MatrixClient matrixClient = new MatrixClient();
+        matrixClient.main(args);
+    }
+}
